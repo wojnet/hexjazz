9394dda475526a76b346f564e853239c5d39d802
wojnet <wojnetto@gmail.com>
2025-10-20 07:53:43 +0200

    feat: changed guess colors

--- src/components/GameBoard/Rows/BoardCell.tsx
+++ src/components/GameBoard/Rows/BoardCell.tsx
@@ -8,7 +8,10 @@ const BoardCell = ({ cell }: BoardCellProps) => {
   return (
     <div
       className="w-11 h-11 bg-slate-200 border-3 border-[#0e2653] rounded-lg flex items-center justify-center"
-      style={{ background: cell.status === "guess" ? "#437057" : cell.status === "almost" ? "#97B067" : cell.status === "close" ? "#E3DE61" : "" }}
+      style={{ background: cell.status === "guess" ? "#6BBF62"
+        : cell.status === "almost" ? "#B5EB9A"
+        : cell.status === "close" ? "#F6F193"
+        : "" }}
     >
       <p className="rubik-bold text-2xl text-[#0e2653]">
         { cell.value.toUpperCase() }
